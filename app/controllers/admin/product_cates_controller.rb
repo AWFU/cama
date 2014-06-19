d51a9734d0b1391ae6ec4ab01ef997451b86b9c5
Hana Hsu <kobanae@gmail.com>
2014-06-19 21:33:53 +0800

    test indentation

--- app/controllers/admin/product_cates_controller.rb
+++ app/controllers/admin/product_cates_controller.rb
@@ -1,5 +1,5 @@
 class Admin::ProductCatesController < AdminController
-  before_action :set_product_cate, only: [:destroy]
+	before_action :set_product_cate, only: [:destroy]
 
 	def index
 		@product_cate = ProductCate.new
@@ -12,21 +12,21 @@ class Admin::ProductCatesController < AdminController
 
 	def create
 		@product_cate = ProductCate.new(product_cate_params)
-    @product_cate.save
+		@product_cate.save
 
-    respond_to do |format|
-      format.html { redirect_to :back }
-    end
+		respond_to do |format|
+			format.html { redirect_to :back }
+		end
 	end
 
 	private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product_cate
-      @product_cate = ProductCate.find(params[:id])
-    end
+		# Use callbacks to share common setup or constraints between actions.
+		def set_product_cate
+			@product_cate = ProductCate.find(params[:id])
+		end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def product_cate_params
-      params.require(:product_cate).permit(:name)
-    end
+		# Never trust parameters from the scary internet, only allow the white list through.
+		def product_cate_params
+			params.require(:product_cate).permit(:name)
+		end
 end
