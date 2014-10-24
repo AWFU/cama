c0c550c7cce26775bed8d9617aa4610617e89efc
Adam <adam@summers.com.tw>
2014-10-24 18:55:57 +0800

    add_recaptcha_key

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,9 +1,14 @@
 Recaptcha.configure do |config|
 
-  if Rails.env.production? #Temporary: cama.summers.com.tw
-
+  if Rails.env.production? 
+    #Temporary: cama.summers.com.tw
     config.public_key  = '6Lc_EPoSAAAAAKefE6nMepworHqRe0wtWdJtf7d8'
     config.private_key = '6Lc_EPoSAAAAAKoNHBzl3XCWhtK2ZT3d_tzNn1El'
+    
+    # li664-20.members.linode.com
+    # config.public_key  = '6LfjHfwSAAAAAE0fEIAVzc61UExdBtj_W2DfsAjX'
+    # config.private_key = '6LfjHfwSAAAAAN3tJ_hpCc8L8yMwQaxB7ZnrP3gi'
+
     #config.proxy = 'http://myproxy.com.au:8080'
   else 
     #Temporary: www.camacafe.com, because localhost is ok
