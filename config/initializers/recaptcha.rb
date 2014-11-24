526563c7f329889d84bb402d37664f3f771247e1
Adam <adam@summers.com.tw>
2014-11-24 15:04:53 +0800

    add username display

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -2,12 +2,12 @@ Recaptcha.configure do |config|
 
   if Rails.env.production? 
     #Temporary: cama.summers.com.tw
-    config.public_key  = '6Lc_EPoSAAAAAKefE6nMepworHqRe0wtWdJtf7d8'
-    config.private_key = '6Lc_EPoSAAAAAKoNHBzl3XCWhtK2ZT3d_tzNn1El'
+    # config.public_key  = '6Lc_EPoSAAAAAKefE6nMepworHqRe0wtWdJtf7d8'
+    # config.private_key = '6Lc_EPoSAAAAAKoNHBzl3XCWhtK2ZT3d_tzNn1El'
     
     # li664-20.members.linode.com
-    # config.public_key  = '6LfjHfwSAAAAAE0fEIAVzc61UExdBtj_W2DfsAjX'
-    # config.private_key = '6LfjHfwSAAAAAN3tJ_hpCc8L8yMwQaxB7ZnrP3gi'
+    config.public_key  = '6LfjHfwSAAAAAE0fEIAVzc61UExdBtj_W2DfsAjX'
+    config.private_key = '6LfjHfwSAAAAAN3tJ_hpCc8L8yMwQaxB7ZnrP3gi'
 
     #config.proxy = 'http://myproxy.com.au:8080'
   else 
